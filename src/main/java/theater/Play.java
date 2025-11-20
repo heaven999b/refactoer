c05aaf41c5a554423f07481004f5d571c7a3475e
yihaiwen <yihaiwen9@gmail.com>
2025-11-21 00:31:02 +0800

    Task 2.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,31 @@
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ *
+ * @null This tag is required by the CheckStyle configuration.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Constructs a play with a name and type.
+     *
+     * @param name the name of the play
+     * @param type the type of the play (e.g., tragedy or comedy)
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
