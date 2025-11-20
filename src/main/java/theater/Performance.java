c634a1fbe6f93610802d8124a5a9b9effd3f78dd
yihaiwen <yihaiwen9@gmail.com>
2025-11-21 00:33:35 +0800

    Task 1.2

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,32 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Represents a single performance of a play.
+ *
+ * @null This tag is required by the CheckStyle configuration.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
+    /**
+     * Creates a performance with a play ID and the number of audience members.
+     *
+     * @param playID   the ID of the play performed
+     * @param audience the number of seats filled for this performance
+     */
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
 }
+
